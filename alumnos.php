92814212a0a79a9d36701887acc8fbc6d5a9f412
JairDel <delval.jair@gmail.com>
2024-11-22 21:53:23 -0600

    Interfaz alumno

---
+++ alumnos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ST-UACH</title>
+    <link rel="stylesheet" href="src/scss/al_ini.css">
+</head>
+<body>
+    <header>
+        <div class="left">ST-UACH</div>
+        <div class="right">Nombre</div>
+    </header>
+
+    <div class="container">
+        <ul class="options">
+            <li><a href="#">Solicitar tutoría</a></li>
+            <li><a href="#">Solicitar cambio de tutor</a></li>
+            <li><a href="#">Agenda</a></li>
+            <li><a href="#">Evaluar sesión de tutoría</a></li>
+        </ul>
+    </div>
+</body>
+</html>
