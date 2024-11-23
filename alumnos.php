12bca852223bb6c26742abf658823b47d9c16084
JairDel <delval.jair@gmail.com>
2024-11-23 12:08:08 -0600

    Interfaz cambio de tutor añadida

--- alumnos.php
+++ alumnos.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ST-UACH</title>
-    <link rel="stylesheet" href="src/scss/al_ini.css">
+    <link rel="stylesheet" href="src/scss/alumno_ini.css">
 </head>
 <body>
     <header>
@@ -15,7 +15,7 @@
     <div class="container">
         <ul class="options">
             <li><a href="#">Solicitar tutoría</a></li>
-            <li><a href="#">Solicitar cambio de tutor</a></li>
+            <li><a href="cambio_tutor.php">Solicitar cambio de tutor</a></li> 
             <li><a href="#">Agenda</a></li>
             <li><a href="#">Evaluar sesión de tutoría</a></li>
         </ul>
