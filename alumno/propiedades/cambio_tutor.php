42c1fe459b62c124e5fe3aa6d0bbcceea57c97f4
IrvinMora13 <irvinmora139@gmail.com>
2024-11-24 13:05:05 -0600

    Update cambio_tutor.php

--- alumno/propiedades/cambio_tutor.php
+++ alumno/propiedades/cambio_tutor.php
@@ -1,5 +1,6 @@
 <?php
     require "../../includes/funciones.php";
+    require "../../includes/config/database.php";
 
     $auth = estaAutenticado();
 
@@ -8,6 +9,41 @@
     }
 
     incluirTemplate('header');
+
+    $db = conectarDB();
+
+    $matricula = $_SESSION["matricula"];
+
+    $query = "SELECT * FROM asignaciones WHERE estudiante_matricula = '$matricula'";
+    $resultado = mysqli_query($db, $query);
+    $usuario = mysqli_fetch_assoc($resultado);
+    $tutorMatricula = $usuario['tutor_matricula'];
+
+    $query2 = "SELECT nombre_completo FROM usuarios WHERE matricula = '$tutorMatricula'";
+    $consulta2 = mysqli_query($db, $query2);
+    $tutor = mysqli_fetch_assoc($consulta2);
+
+    $nuevoTutor = '';
+    $razonCambio = '';
+
+    $errores = [];
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $nuevoTutor = $_POST['nuevo-tutor'];
+        $razonCambio = $_POST['razon-cambio'];
+
+        
+        
+        if (empty($errores)) {
+        
+            $query3 = "INSERT INTO solicitud_tutor (id,nombre,motivo) VALUES ('$matricula', '$nuevoTutor','$razonCambio')";
+            $consulta3 = mysqli_query($db, $query3);
+            var_dump($consulta3);
+            
+            
+
+        }
+    }
 ?>
 
 <body>
@@ -15,9 +51,9 @@
         <div class="form-container">
             <h2>Cambio de tutor</h2>
             <p class="subtitle">Actualizar información</p>
-            <form>
+            <form method="POST">
                 <label for="tutor-actual">Nombre del tutor actual</label>
-                <input type="text" id="tutor-actual" name="tutor-actual">
+                <span><?php echo isset($tutor['nombre_completo']) ? htmlspecialchars($tutor['nombre_completo']) : 'Tutor Anónimo'; ?></span>
 
                 <label for="nuevo-tutor">Nuevo tutor</label>
                 <input type="text" id="nuevo-tutor" name="nuevo-tutor">
